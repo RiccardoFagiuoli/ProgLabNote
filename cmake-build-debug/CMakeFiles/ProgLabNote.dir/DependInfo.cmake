b67c82d478798100150958fa83c53b4df9cdd016
Riccardo Fagiuoli <riccardo.fagiuoli@stud.unifi.it>
2025-02-24 19:55:38 +0100

    TODO Important Update

---
+++ cmake-build-debug/CMakeFiles/ProgLabNote.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Riccardo/Documents/Documenti/UNIFI/I Anno/I Semestre/Fondamenti di Informatica/ProgLabNote/Collezioni.cpp" "CMakeFiles/ProgLabNote.dir/Collezioni.cpp.obj" "gcc" "CMakeFiles/ProgLabNote.dir/Collezioni.cpp.obj.d"
+  "C:/Users/Riccardo/Documents/Documenti/UNIFI/I Anno/I Semestre/Fondamenti di Informatica/ProgLabNote/Importanti.cpp" "CMakeFiles/ProgLabNote.dir/Importanti.cpp.obj" "gcc" "CMakeFiles/ProgLabNote.dir/Importanti.cpp.obj.d"
+  "C:/Users/Riccardo/Documents/Documenti/UNIFI/I Anno/I Semestre/Fondamenti di Informatica/ProgLabNote/Note.cpp" "CMakeFiles/ProgLabNote.dir/Note.cpp.obj" "gcc" "CMakeFiles/ProgLabNote.dir/Note.cpp.obj.d"
+  "C:/Users/Riccardo/Documents/Documenti/UNIFI/I Anno/I Semestre/Fondamenti di Informatica/ProgLabNote/main.cpp" "CMakeFiles/ProgLabNote.dir/main.cpp.obj" "gcc" "CMakeFiles/ProgLabNote.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ProgLabNote.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProgLabNote.dir/Collezioni.cpp.obj"
+  "CMakeFiles/ProgLabNote.dir/Collezioni.cpp.obj.d"
+  "CMakeFiles/ProgLabNote.dir/Importanti.cpp.obj"
+  "CMakeFiles/ProgLabNote.dir/Importanti.cpp.obj.d"
+  "CMakeFiles/ProgLabNote.dir/Note.cpp.obj"
+  "CMakeFiles/ProgLabNote.dir/Note.cpp.obj.d"
+  "CMakeFiles/ProgLabNote.dir/main.cpp.obj"
+  "CMakeFiles/ProgLabNote.dir/main.cpp.obj.d"
+  "ProgLabNote.exe"
+  "ProgLabNote.exe.manifest"
+  "ProgLabNote.pdb"
+  "libProgLabNote.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProgLabNote.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
